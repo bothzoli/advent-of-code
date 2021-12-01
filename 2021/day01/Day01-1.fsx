116f5bb1955e6e5fb3a8824ced09b26daa72c5d9
bothzoli <bothzoli@gmail.com>
2021-12-01 14:17:28 +0100

    CleanUp: Advent of Code - 2021 / Day 1

--- 2021/day01/Day01-1.fsx
+++ 2021/day01/Day01-1.fsx
@@ -1,25 +1,15 @@
 open System.IO
 
-let input = File.ReadAllLines("input.txt") |> Array.map (int)
+let input = File.ReadAllLines("input.txt") |> Array.map int
 
-let isIncrease (before, after) =
-    if (after - before) > 0 then true
-    else false
-
-isIncrease(100, 200)
-isIncrease(200, 100)
-isIncrease(200, 200)
+let isIncrease (before, after) = if after > before then 1 else 0
 
 input
 |> Seq.pairwise
-|> Seq.sumBy (fun e -> if isIncrease e then 1 else 0)
-
-let isIncreaseThree (before, after) =
-    let beforeSum = Array.sum before
-    let afterSum = Array.sum after
-    isIncrease(beforeSum, afterSum)
+|> Seq.sumBy isIncrease
 
 input
 |> Seq.windowed 3
+|> Seq.map Array.sum
 |> Seq.pairwise
-|> Seq.sumBy (fun e -> if isIncreaseThree e then 1 else 0)
+|> Seq.sumBy isIncrease
